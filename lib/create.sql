e2f93d69760af8b8defb11c12aacd8befef05b1d
Kelly Koome <koomekelly7@gmail.com>
2025-05-20 15:43:34 +0300

    Add initial bear data to insert.sql

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    sex TEXT NOT NULL,
+    color TEXT NOT NULL,
+    temperament TEXT NOT NULL,
+    alive BOOLEAN NOT NULL
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 3, 'M', 'brown', 'friendly', 1),
+('Rowdy', 5, 'M', 'black', 'grumpy', 0),
+('Tabitha', 4, 'M', 'gray', 'curious', 1),
+('Sergeant Brown', 2, 'M', 'brown', 'playful', 1),
+('Melissa', 6, 'M', 'yellow', 'lazy', 1),
+('Grinch', 7, 'M', 'black', 'friendly', 1),
+('Wendy', 1, 'F', 'brown', 'playful', 1),
+(NULL, 4, 'F', 'golden', 'curious', 0);
